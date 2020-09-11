1b030fe808118f598e69e6179a5591d1526bf11e
Elizaveta Konovkina <70512444+ElizavetaKkk@users.noreply.github.com>
2020-09-12 02:20:25 +0300

    done the lesson4

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -320,42 +320,70 @@ fun roman(n: Int): String {
  * Например, 375 = "триста семьдесят пять",
  * 23964 = "двадцать три тысячи девятьсот шестьдесят четыре"
  */
-fun russian(n: Int): String = TODO()
+fun russian(n: Int): String {
+    var s = halfTheNumber(n / 1000)
+    s = s.trim()
+    if (s.isNotEmpty()) {
+        when (n / 1000 % 10) {
+            1 -> if (n / 100 % 10 != 1) {
+                s = s.dropLast(4)
+                s += "одна тысяча "
+            } else s += " тысяч "
+            2 -> if (n / 100 % 10 != 1) {
+                s = s.dropLast(3)
+                s += "две тысячи "
+            } else s += " тысяч "
+            in 3..4 -> s += " тысячи "
+            else -> s += " тысяч "
+        }
+    }
+    s += halfTheNumber(n % 1000)
+    return s.trim()
+}
 
-/*fun writing(n: Int): String {
-    val numerals = listOf("один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять", "десять")
-    val dozens = listOf("надцать", "десят", "дцать")
-    val hundreds = listOf("ста", "сот")
-    val thousand = "тысяч"
-    var n1 = n % 100
-    var s = " "
-    var i = 2
-    while (i > -1) {
-        var a = (n1 / 10.0.pow(i) % 10).toInt()
-        when (i) {
-            2 -> when (a) {
-                1 -> "сто "
-                2 -> "двести "
-                else -> {
-                    s += numerals[a - 1]
-                    s += if (a in 3..4) hundreds[0]
-                    else hundreds[1]
-                    s += " "
-                }
-            }
-            1 -> when (a) {
-                1 -> when (a % 10) {
-                    0 -> "десять"
-                    1 -> "одиннадцать "
-                    2 -> "двенадцать "
-                    3 -> "тринадцать "
-                    else -> numerals[a % 10 - 1].dropLast(1) + dozens[0] + " "
-                }
-                in 2..3 -> numerals[a - 1] + dozens[2]
-                4 -> "сорок"
-                in 5..8 -> numerals[a - 1]
+fun halfTheNumber(x: Int): String {
+    val numbers = listOf(
+        "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять", "десять",
+        "сорок", "девяносто", "сто", "двести"
+    )
+    var s = ""
+    var a = x / 100
+    if (x >= 100) {
+        when (a) {
+            in 1..2 -> s += numbers[11 + a]
+            else -> {
+                s += numbers[a - 1]
+                s += if (a in 3..4) "ста"
+                else "сот"
             }
         }
-        i--
+        s += " "
     }
-}*/
+    a = x / 10 % 10
+    if (a != 0) {
+        when (a) {
+            1 -> {
+                val b = x % 10
+                s += when (b) {
+                    0 -> numbers[9]
+                    1, 3 -> numbers[b - 1]
+                    2 -> "две"
+                    else -> numbers[b - 1].dropLast(1)
+                }
+                if (b != 0) s += "надцать"
+                return s
+            }
+            4 -> s += numbers[10]
+            9 -> s += numbers[11]
+            else -> {
+                s += numbers[a - 1]
+                s += if (a in 5..8) "десят"
+                else "дцать"
+            }
+        }
+        s += " "
+    }
+    a = x % 10
+    if (a != 0) s += numbers[a - 1]
+    return s
+}
